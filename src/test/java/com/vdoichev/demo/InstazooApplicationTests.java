19be72e055174c2c9fcf19e62e38f83164d8c7cc
Дойчев Вячеслав <40699294+vdoichev@users.noreply.github.com>
2022-12-19 15:43:50 +0200

    Инициализация проекта и добавление моделей

---
+++ src/test/java/com/vdoichev/demo/InstazooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vdoichev.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstazooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
