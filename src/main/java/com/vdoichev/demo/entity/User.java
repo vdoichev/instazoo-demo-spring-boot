abe87543baf88bc0ca2b955e051a9157074bada2
Дойчев Вячеслав <40699294+vdoichev@users.noreply.github.com>
2022-12-19 17:44:18 +0200

    Добавил кастомный сервис для пользователя

--- src/main/java/com/vdoichev/demo/entity/User.java
+++ src/main/java/com/vdoichev/demo/entity/User.java
@@ -3,17 +3,16 @@ package com.vdoichev.demo.entity;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.vdoichev.demo.entity.enums.ERole;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Data
 @Entity
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -34,7 +33,7 @@ public class User {
     @ElementCollection(targetClass = ERole.class)
     @CollectionTable(name = "user_role",
             joinColumns = @JoinColumn(name = "user_id"))
-    private Set<ERole> role = new HashSet<>();
+    private Set<ERole> roles = new HashSet<>();
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY,
             mappedBy = "user", orphanRemoval = true)
     private List<Post> posts = new ArrayList<>();
@@ -42,8 +41,55 @@ public class User {
     @Column(updatable = false)
     private LocalDateTime createdDate;
 
+    @Transient
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public User() {
+    }
+
+    public User(Long id,
+                String username,
+                String email,
+                String password,
+                Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
     @PrePersist
     protected void onCreate() {
         this.createdDate = LocalDateTime.now();
+    }
+
+    /**
+     * SECURITY
+     */
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
