853424b74b7e8e57d447ed23fc68c565a6608a57
Дойчев Вячеслав <40699294+vdoichev@users.noreply.github.com>
2022-12-20 09:21:02 +0200

    Добавил хранилище токена

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from "./material-module";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {authInterceptorProviders} from "./helper/auth-interceptor.service";
 
 @NgModule({
   declarations: [
@@ -10,9 +14,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- client/src/app/services/token-storage.service.ts
+++ client/src/app/services/token-storage.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+
+const TOKEN_KEY = 'auth-token';
+const USER_KEY = 'auth-user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenStorageService {
+  constructor() {
+  }
 
-  constructor() { }
+  public saveToken(token: string): void {
+    window.sessionStorage.removeItem(TOKEN_KEY);
+    window.sessionStorage.setItem(TOKEN_KEY, token);
+  }
+
+  public getToken(): string {
+    return window.sessionStorage.getItem(TOKEN_KEY);
+  }
+
+  public saveUser(user): void {
+    window.sessionStorage.removeItem(USER_KEY);
+    window.sessionStorage.setItem(USER_KEY, user);
+  }
+
+  public getUser(): string {
+    return JSON.parse(window.sessionStorage.getItem(USER_KEY));
+  }
+
+  public logOut(): void {
+    window.sessionStorage.clear();
+    window.location.reload();
+  }
 }
